83a64ebd4a2be83b68318be045dd4b510cd40daa
Lílian Rangel <105670314+lilian-rangel@users.noreply.github.com>
2023-07-31 15:42:31 -0300

    Add files via upload

---
+++ sum_float.py
@@ -0,0 +1,9 @@
+sum = 0
+y = float(input())
+
+while y != -1:
+    sum += y
+    y = y = float(input())
+print("{0:3.2f}".format(sum))
+
+#programa le números até digitar -1 e imprime a soma dos número com 2 casas decimais.
